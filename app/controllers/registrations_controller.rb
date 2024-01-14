5e0fb0db550ba9772a116873b2f74942f08abca5
Indrit Caca <cacaindrit81@gmail.com>
2024-01-14 16:01:59 +0100

    Final Changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,18 +1,26 @@
 class RegistrationsController < ApplicationController
   def new
-    @title = "Sign Up"
-    @user = User.new
+    if current_user.nil?
+      @title = "Sign Up"
+      @user = User.new
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def create
-    @user = User.new(user_signup_params)
-    if @user.save
-      @current_user = current_user
-      flash[:notice] = "User registered successfully!"
-      redirect_to login_path
+    if current_user.nil?
+      @user = User.new(user_signup_params)
+      if @user.save
+        @current_user = current_user
+        flash[:notice] = "User registered successfully!"
+        redirect_to login_path
+      else
+        flash[:alert] = "User could not be registered!"
+        render :new
+      end
     else
-      flash[:alert] = "User could not be registered!"
-      render :new
+      redirect_to user_path(current_user)
     end
   end
 
